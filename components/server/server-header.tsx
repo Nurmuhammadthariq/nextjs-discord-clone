f5ad892321f5f48c52eadbeab44c6dc1ddd09dab
thoriq.noer <thoriq.noer@gmail.com>
2024-06-02 16:45:16 +0700

    feat: create feature member settings

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -80,7 +80,7 @@ export const ServerHeader = ({ server, role }: ServerHeaderProps) => {
 				)}
 				{isAdmin && (
 					<DropdownMenuItem
-						onClick={() => {}}
+						onClick={() => onOpen("members", { server })}
 						className='px-3 py-2 text-sm cursor-pointer'
 					>
 						Manage Members
